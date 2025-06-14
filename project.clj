94377edca438b453efbe59b27f75a826a760bfd5
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2025-06-14 07:23:14 -0300

    prepare project configuration file

--- project.clj
+++ project.clj
@@ -1,12 +1,42 @@
 (defproject escriba "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
+  :description "Managing and organizing the print queue for thermal printers"
 
-  :url "http://example.com/FIXME"
+  :url "https://github.com/macielti/escriba"
 
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
 
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]
 
-  :repl-options {:init-ns escriba.core})
+                 [net.clojars.macielti/common-clj "43.74.74"]
+                 [net.clojars.macielti/postgresql-component "2.2.5"]
+                 [net.clojars.macielti/service-component "2.4.2"]]
+
+  :profiles {:dev {:plugins        [[lein-shell "0.5.0"]
+                                    [com.github.liquidz/antq "RELEASE"]
+                                    [com.github.clojure-lsp/lein-clojure-lsp "2.0.9"]]
+
+                   :resource-paths ["resources"]
+
+                   :test-paths     ["test/unit" "test/integration" "test/helpers"]
+
+                   :dependencies   [[net.clojars.macielti/common-test-clj "5.2.4"]
+                                    [com.github.igrishaev/pg2-migration "0.1.40"]
+                                    [http-kit.fake/http-kit.fake "0.2.2"]
+                                    [nubank/matcher-combinators "3.9.1"]
+                                    [danlentz/clj-uuid "0.2.0"]
+                                    [hashp "0.2.2"]]
+
+                   :injections     [(require 'hashp.core)]
+
+                   :aliases        {"clean-ns"     ["clojure-lsp" "clean-ns" "--dry"] ;; check if namespaces are clean
+                                    "format"       ["clojure-lsp" "format" "--dry"] ;; check if namespaces are formatted
+                                    "diagnostics"  ["clojure-lsp" "diagnostics"] ;; check if project has any diagnostics (clj-kondo findings)
+                                    "lint"         ["do" ["clean-ns"] ["format"] ["diagnostics"]] ;; check all above
+                                    "clean-ns-fix" ["clojure-lsp" "clean-ns"] ;; Fix namespaces not clean
+                                    "format-fix"   ["clojure-lsp" "format"] ;; Fix namespaces not formatted
+                                    "lint-fix"     ["do" ["clean-ns-fix"] ["format-fix"]] ;; Fix both
+                                    }}}
+
+  :main beagle-graalvm.components)
