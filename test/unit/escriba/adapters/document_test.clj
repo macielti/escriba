aa111dd15b5175b6390f178621999e3e0bc9c0af
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2026-02-23 20:59:03 -0300

    add more unit tests

--- test/unit/escriba/adapters/document_test.clj
+++ test/unit/escriba/adapters/document_test.clj
@@ -1,6 +1,9 @@
 (ns escriba.adapters.document-test
   (:require [clojure.test :refer [is testing]]
+            [common-test-clj.helpers.schema :as helpers.schema]
             [escriba.adapters.document :as adapters.document]
+            [escriba.models.command :as models.command]
+            [escriba.models.document :as models.document]
             [fixtures.document]
             [matcher-combinators.test :refer [match?]]
             [schema.test :as s]))
@@ -13,3 +16,12 @@
                  :created-at inst?
                  :status     :requested}
                 (adapters.document/wire->internal fixtures.document/wire-document)))))
+
+(s/deftest document->datalevin-test
+  (testing "Given a internal document we should be able to convert it to datalevin"
+    (let [internal-commands [(helpers.schema/generate models.command/Command {:type :cut})]
+          internal-document (helpers.schema/generate models.document/Document {:commands internal-commands})]
+      (is (match? {:document/id         uuid?
+                   :document/status     keyword?
+                   :document/created-at inst?}
+                  (adapters.document/document->datalevin internal-document))))))
