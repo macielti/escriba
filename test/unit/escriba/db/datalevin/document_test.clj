becd6c3ea187cbfd61b7023a8f9691c9feaca43e
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2026-02-24 19:30:11 -0300

    add unit test to insert-document-with-commands

---
+++ test/unit/escriba/db/datalevin/document_test.clj
@@ -0,0 +1,27 @@
+(ns escriba.db.datalevin.document-test
+  (:require [clojure.test :refer :all]
+            [common-test-clj.helpers.schema :as helpers.schema]
+            [datalevin.mock :as database.mock]
+            [escriba.db.datalevin.config :as database.config]
+            [escriba.db.datalevin.document :as database.document]
+            [escriba.models.command :as models.command]
+            [escriba.models.document :as models.document]
+            [fixtures.document]
+            [java-time.api :as jt]
+            [matcher-combinators.test :refer [match?]]
+            [schema.test :as s]))
+
+(s/deftest insert-document-with-commands!-test
+  (testing "We should be able to persist a Document entity model with it's documents"
+    (let [database-connection (database.mock/database-connection-for-unit-tests! database.config/schema)
+          internal-commands [(helpers.schema/generate models.command/Command {:type :cut})]
+          internal-document (helpers.schema/generate models.document/Document {:id       fixtures.document/document-id
+                                                                               :commands internal-commands})]
+      (is (match? {:commands   [{:document-id fixtures.document/document-id
+                                 :id          uuid?
+                                 :index       integer?
+                                 :type        :cut}]
+                   :created-at jt/instant?
+                   :id         fixtures.document/document-id
+                   :status     keyword?}
+                  (database.document/insert-document-with-commands! internal-document database-connection))))))
